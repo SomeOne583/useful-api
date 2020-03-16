28960399f180c11406e231b44fc6fda44dc50311
SomeOne583 <janel.fantasma2002@gmail.com>
2020-03-15 19:14:58 -0600

    Fixed str treated as int

--- app/controllers/api/v1/primes_controller.rb
+++ app/controllers/api/v1/primes_controller.rb
@@ -1,7 +1,7 @@
 module Api::V1
     class PrimesController < ApplicationController
         def index
-            @primes = PrimeCalculator.new(params[:amount])
+            @primes = PrimeCalculator.new(params[:amount].to_i)
 
             render json: @primes
         end
